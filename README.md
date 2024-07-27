224ed487a8be9f34085f7e59500ac893662c8b73
michaljarnot <m.jarnot@yahoo.com>
2024-07-27 22:02:08 +0200

    feat: make minimal working example

---
+++ README.md
@@ -0,0 +1,10 @@
+# llama.cpp-ts example
+
+This is an example of how to use the `llama.cpp-ts` package to interact with language models loaded through `llama.cpp`.
+
+## Installation
+
+Before installation download any gguf model that your machine can handle. For example this one [Meta-Llama-3.1-8B-Instruct-Q3_K_S.gguf](https://huggingface.co/bullerwins/Meta-Llama-3.1-8B-Instruct-GGUF/resolve/main/Meta-Llama-3.1-8B-Instruct-Q3_K_S.gguf) and put it in `/models` folder.
+
+1. `yarn install`
+2. `yarn start`
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
-import { Llama } from 'llama.cpp-ts';
+import { Llama, TokenStream } from 'llama.cpp-ts'
 
-const llama = new Llama();
-const initialized = llama.initialize('./path/to/your/model.gguf');
+// sync
+// const llama = new Llama()
+// const initialized = llama.initialize(__dirname + '/../models/Meta-Llama-3.1-8B-Instruct-Q3_K_S.gguf')
+//
+// if (initialized) {
+//     const response: string = llama.runQuery("Tell me a story.", 100)
+//     console.log(response)
+// } else {
+//     console.error("Failed to initialize the model.")
+// }
 
-if (initialized) {
-    const response: string = llama.runQuery("Tell me a joke about programming.", 100);
-    console.log(response);
-} else {
-    console.error("Failed to initialize the model.");
+// stream
+async function main() {
+    const llama = new Llama()
+    const initialized: boolean = llama.initialize(__dirname + '/../models/Meta-Llama-3.1-8B-Instruct-Q3_K_S.gguf')
+
+    if (initialized) {
+        const tokenStream: TokenStream = llama.runQueryStream("Explain quantum computing", 200)
+
+        while (true) {
+            const token: string | null = await tokenStream.read()
+            if (token === null) break
+            process.stdout.write(token)
+        }
+    } else {
+        console.error("Failed to initialize the model.")
+    }
 }
+
+main().catch(console.error)
